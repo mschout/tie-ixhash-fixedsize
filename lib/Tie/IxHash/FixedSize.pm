106c90247bb7100cb0f7ff242418ba90076470d3
Michael Schout <mschout@lothlorien.(none)>
2008-09-03 12:26:24 -0500

    add pod documentation

--- lib/Tie/IxHash/FixedSize.pm
+++ lib/Tie/IxHash/FixedSize.pm
@@ -6,7 +6,7 @@ use base 'Tie::IxHash';
 
 our $VERSION = 0.10;
 
-# location of size field in @$self
+# location of size field in @$self. Tie::IxHash uses 0-3
 use constant SIZE_IX => 4;
 
 sub TIEHASH {
@@ -42,37 +42,45 @@ __END__
 
 =head1 NAME
 
-Tie::IxHash::FixedSize - Perl extension for blah blah blah
+Tie::IxHash::FixedSize - Tie::IxHash with a fixed maximum size
 
 =head1 SYNOPSIS
 
   use Tie::IxHash::FixedSize;
-  blah blah blah
+
+  tie my %h, 'Tie::IxHash::FixedSize', {size => 3},
+    one   => 1,
+    two   => 2,
+    three => 3;
+
+  print join ' ', keys %h;   # prints 'one two three'
+
+  $h{four} = 4;  # key 'one' is removed, 'four' is added
+
+  print join ' ', keys %h;   # prints 'two three four'
+
+=head1 ABSTRACT
+
+Hashes tied to Tie::IxHash::FixedSize will only hold a fixed maximum number of
+keys before automatically removing old keys.
 
 =head1 DESCRIPTION
 
-Stub documentation for Tie::IxHash::FixedSize, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+Hashes tied with Tie::IxHash::FixedSize behave exactly like normal Tie::IxHash
+hashes, except the maximum number of keys that can be held by the hash is
+limited by a specified C<size>.  Once the number of keys in the hash exceeds
+this size, the oldest keys in the hash will automatically be removed.
 
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
+The C<size> parameter to C<tie()> specifies the maximum number of keys that the
+hash can hold. When the hash exceeds this number of keys, the first entries in
+the hash will automatically be removed until the number of keys in the hash
+does not exceed the size parameter.  If no size parameter is given, then the
+hash will behave exactly like a plan Tie::IxHash, and the number of keys will
+not be limited.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<Tie::IxHash>
 
 =head1 AUTHOR
 
@@ -82,9 +90,6 @@ Michael Schout, E<lt>mschout@E<gt>
 
 Copyright (C) 2008 by Michael Schout
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.8 or,
-at your option, any later version of Perl 5 you may have available.
-
-
-=cut
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself, either Perl version 5.8.8 or, at your option,
+any later version of Perl 5 you may have available.
